4140b86ab3a6bef928d355f248ee88319e0f873b
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-25 13:44:09 -0500

    fix(main): format code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config.js");
 
 const remotes = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
   return {
-    harryPotter : `harryPotter@${process.env.URL_APP_HARRY_POTTER}/_next/static/${location}/remoteEntry.js`,
-    rickAndMorty : `rickAndMorty@${process.env.URL_APP_RICK_AND_MORTY}/_next/static/${location}/remoteEntry.js`,
+    harryPotter: `harryPotter@${process.env.URL_APP_HARRY_POTTER}/_next/static/${location}/remoteEntry.js`,
+    rickAndMorty: `rickAndMorty@${process.env.URL_APP_RICK_AND_MORTY}/_next/static/${location}/remoteEntry.js`,
   };
 };
 
@@ -14,14 +14,17 @@ const nextConfig = {
   i18n,
   reactStrictMode: true,
   images: {
-    domains: ["ik.imagekit.io","rickandmortyapi.com"],
+    domains: ["ik.imagekit.io", "rickandmortyapi.com"],
   },
   webpack(config, options) {
+    config.output.publicPath = "auto";
+
     config.plugins.push(
       new NextFederationPlugin({
         name: "main",
         filename: "static/chunks/remoteEntry.js",
         remotes: remotes(options.isServer),
+        shared: ["react", "react-dom"],
       })
     );
     return config;
