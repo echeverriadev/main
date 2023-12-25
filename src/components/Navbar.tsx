a79692a8a4e237c8cc61650f6cb1a9a9202e3911
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-25 13:45:48 -0500

    feat(main): i18n (#3)
    * feat(main): i18n installed and configurated
    
    * feat(main): page and components translations
    
    * feat(main): creating button to change language
    
    * fix(main): clean code
    
    * fix(main): format code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 import { ActiveLink } from "./ActiveLink";
 import styles from "../styles/Navbar.module.css";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
+  const {locale, asPath, push} = useRouter();
+
+  const handleLocale = () => {
+    const newLocale = locale === 'en' ? 'es' : 'en';
+    push(asPath, asPath, { locale: newLocale });
+  }
+
   return (
     <nav className={styles["navbar-container"]}>
       <ActiveLink href="/harry-potter" label="Harry Potter" />
       <ActiveLink href="/rick-and-morty" label="Rick y Morty" />
+      <button onClick={handleLocale}>{locale?.toUpperCase()}</button>
     </nav>
   );
 };
