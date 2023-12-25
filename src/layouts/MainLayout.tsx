198a8a9b6f1d3fdb4baa0546b52bfc8cea0323de
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-25 16:09:43 -0500

    feat(main): config jest to unit test

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "../components/Navbar";
 import { useTranslation } from 'next-i18next';
 import { Inter } from "next/font/google";
 import Head from "next/head";
