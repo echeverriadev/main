3eb3ebd8a67fd57f7f7228228b8192c1f2d80909
Carlos Echeverría <55212109+echeverriadev@users.noreply.github.com>
2023-12-26 15:20:42 -0500

    fix(main): update styles import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://harry-potter-gules-ten.vercel.app/_next/static/css/0141e9d1d922fb63.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
